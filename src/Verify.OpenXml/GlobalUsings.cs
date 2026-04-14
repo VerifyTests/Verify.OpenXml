3c0b77a3119a7e2499f8d94095ef7b321daf84f1
Simon Cropp <simon.cropp@gmail.com>
2026-04-14 12:31:51 +1000

    Update GlobalUsings.cs

--- src/Verify.OpenXml/GlobalUsings.cs
+++ src/Verify.OpenXml/GlobalUsings.cs
@@ -1,6 +1,3 @@
-// Global using directives
-
-global using Argon;
 global using DeterministicIoPackaging;
 global using DocumentFormat.OpenXml.Packaging;
 global using DocumentFormat.OpenXml.Spreadsheet;
